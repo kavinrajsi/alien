ee3f08fc2514dd53d0b775814c1a8e9692468e3e
kavinrajsi <we@sikavinraj.me>
2020-04-20 09:44:19 +0530

    about-us page base init

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,184 +1,192 @@
-// console.log("Allo Allo!");
+// console.log('Allo Allo!');
 
-$(document).ready(function() {
-  var project1 = $('.we-let-our-work').offset();
-  var $window = $(window);
+$(document).ready(function () {
+    if ($('.we-let-our-work')[0]) {
+    var project1 = $('.we-let-our-work').offset();
+    var $window = $(window);
 
-  $window.scroll(function() {
-      if ( $window.scrollTop() >= project1.top) {
-          $("body").removeClass().addClass("wework-section");
+    $window.scroll(function () {
+      if ($window.scrollTop() >= project1.top) {
+        $('body').removeClass().addClass('wework-section');
       }
-  });
+    });
+  }
 });
-
 
 // init controller
 const controller = new ScrollMagic.Controller();
+  if ($('.hero-unit')[0]) {
+  $('.hero-unit').each(function (i) {
+    var animateIn = new TimelineMax();
+    var imgOvarly = $(this).find('.page-title, .page-title-secondary');
+    console.log(imgOvarly);
+    animateIn.staggerFromTo(
+      imgOvarly,
+      1,
+      {
+        opacity: 0,
+        y: 30,
+        transformOrigin: 'bottom',
+        ease: Power4.easeOut,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        transformOrigin: 'bottom',
+        ease: Power4.easeOut,
+      },
+      0.5
+    );
 
-$(".hero-unit").each(function (i) {
-  var animateIn = new TimelineMax();
-  var imgOvarly = $(this).find(".page-title, .page-title-secondary");
-  console.log(imgOvarly);
-  animateIn.staggerFromTo(
-    imgOvarly,
-    1,
-    {
-      opacity: 0,
-      y: 30,
-      transformOrigin: "bottom",
-      ease: Power4.easeOut,
-    },
-    {
-      opacity: 1,
-      y: 0,
-      transformOrigin: "bottom",
-      ease: Power4.easeOut,
-    },
-    0.5
-  );
+    var scene = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.5,
+      offset: '0',
+    })
+      .setTween(animateIn)
+      .addIndicators()
+      .addTo(controller);
 
-  var scene = new ScrollMagic.Scene({
-    triggerElement: this,
-    triggerHook: 0.5,
-    offset: "0",
-  })
-    .setTween(animateIn)
-    .addIndicators()
-    .addTo(controller);
+    var reverse = scene.reverse();
+    scene.reverse(true);
+  });
+}
 
-  var reverse = scene.reverse();
-  scene.reverse(true);
-});
+if ($('.wedo-content')[0]) {
+  TweenLite.set('.wedo-content', {
+    autoAlpha: 0.3,
+    height: 0,
+  });
+}
 
-TweenLite.set(".wedo-content", {
-  autoAlpha: 0.3,
-  height: 0,
-});
+if ($('.about-what-we-do')[0]) {
+  $('.asp').each(function (i) {
+    const animateIn = gsap.timeline();
+    const imgOvarly = $(this).find('.wedo-content');
+    console.log(imgOvarly);
 
-$(".asp").each(function (i) {
-  const animateIn = gsap.timeline();
-  const imgOvarly = $(this).find(".wedo-content");
-  console.log(imgOvarly);
+    animateIn.staggerFromTo(
+      imgOvarly,
+      1,
+      {
+        opacity: 0,
+        height: 0,
+        y: 30,
+        transformOrigin: 'bottom',
+        ease: Power4.easeOut,
+      },
+      {
+        opacity: 1,
+        height: '100%',
+        y: 0,
+        transformOrigin: 'bottom',
+        ease: Power4.easeOut,
+      },
+      0.23
+    );
 
-  animateIn.staggerFromTo(
-    imgOvarly,
-    1,
-    {
-      opacity: 0,
-      height: 0,
-      y: 30,
-      transformOrigin: "bottom",
-      ease: Power4.easeOut,
-    },
-    {
-      opacity: 1,
-      height: "100%",
-      y: 0,
-      transformOrigin: "bottom",
-      ease: Power4.easeOut,
-    },
-    0.23
-  );
+    // create a scene for each element
+    var wedoList = new ScrollMagic.Scene({
+      // triggerElement: revealElements[i],
+      triggerElement: this,
+      offset: '0',
+      triggerHook: 0.5,
+      duration: '100px',
+    })
+      .setClassToggle(this, 'active')
+      .setTween(animateIn)
+      .addIndicators({
+        name: i + 1,
+        colorTrigger: 'white',
+        colorStart: 'white',
+        colorEnd: 'white',
+        indent: 25,
+      }) // add indicators (requires plugin)
+      .addTo(controller);
 
-  // create a scene for each element
-  var wedoList = new ScrollMagic.Scene({
-    // triggerElement: revealElements[i],
-    triggerElement: this,
-    offset: "0",
-    triggerHook: 0.5,
-    duration: "100px",
-  })
-    .setClassToggle(this, "active")
-    .setTween(animateIn)
-    .addIndicators({
-      name: i + 1,
-      colorTrigger: "white",
-      colorStart: "white",
-      colorEnd: "white",
-      indent: 25,
-    }) // add indicators (requires plugin)
-    .addTo(controller);
+    var reverses = wedoList.reverse();
+    wedoList.reverse(true);
+  });
+}
 
-  var reverses = wedoList.reverse();
-  wedoList.reverse(true);
-});
+if ($('.we-let-our-work')[0]) {
+  $('.we-let-our-work').each(function (i) {
+    var animateIn = new TimelineMax();
+    var imgOvarly = $(this).find('.section-title span, .section-content');
+    console.log(imgOvarly);
+    animateIn.staggerFromTo(
+      imgOvarly,
+      1,
+      {
+        opacity: 0,
+        y: 30,
+        transformOrigin: 'bottom',
+        ease: Power4.easeOut,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        transformOrigin: 'bottom',
+        ease: Power4.easeOut,
+      },
+      0.5
+    );
 
+    var scene = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.5,
+      offset: '0',
+    })
+      .setTween(animateIn)
+      .addIndicators()
+      .addTo(controller);
 
-$(".we-let-our-work").each(function (i) {
-  var animateIn = new TimelineMax();
-  var imgOvarly = $(this).find(".section-title span, .section-content");
-  console.log(imgOvarly);
-  animateIn.staggerFromTo(
-    imgOvarly,
-    1,
-    {
-      opacity: 0,
-      y: 30,
-      transformOrigin: "bottom",
-      ease: Power4.easeOut,
-    },
-    {
-      opacity: 1,
-      y: 0,
-      transformOrigin: "bottom",
-      ease: Power4.easeOut,
-    },
-    0.5
-  );
+    var reverse = scene.reverse();
+    scene.reverse(true);
+  });
+}
 
-  var scene = new ScrollMagic.Scene({
-    triggerElement: this,
-    triggerHook: 0.5,
-    offset: "0",
-  })
-    .setTween(animateIn)
-    .addIndicators()
-    .addTo(controller);
+if ($('.project-list')[0]) {
+  $('.project-list').each(function (i) {
+    var animateIn = new TimelineMax();
+    var imgOvarly = $(this).find('.project-wrapper');
+    console.log(imgOvarly);
+    animateIn.staggerFromTo(
+      imgOvarly,
+      1,
+      {
+        opacity: 0,
+        y: 30,
+        transformOrigin: 'bottom',
+        ease: Power4.easeOut,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        transformOrigin: 'bottom',
+        ease: Power4.easeOut,
+      },
+      0.5
+    );
 
-  var reverse = scene.reverse();
-  scene.reverse(true);
-});
+    var scene = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.5,
+      offset: '0',
+    })
+      .setTween(animateIn)
+      .addIndicators()
+      .addTo(controller);
 
-
-$(".project-list").each(function (i) {
-  var animateIn = new TimelineMax();
-  var imgOvarly = $(this).find(".project-wrapper");
-  console.log(imgOvarly);
-  animateIn.staggerFromTo(
-    imgOvarly,
-    1,
-    {
-      opacity: 0,
-      y: 30,
-      transformOrigin: "bottom",
-      ease: Power4.easeOut,
-    },
-    {
-      opacity: 1,
-      y: 0,
-      transformOrigin: "bottom",
-      ease: Power4.easeOut,
-    },
-    0.5
-  );
-
-  var scene = new ScrollMagic.Scene({
-    triggerElement: this,
-    triggerHook: 0.5,
-    offset: "0",
-  })
-    .setTween(animateIn)
-    .addIndicators()
-    .addTo(controller);
-
-  var reverse = scene.reverse();
-  scene.reverse(true);
-});
+    var reverse = scene.reverse();
+    scene.reverse(true);
+  });
+}
 
 /* last section */
-$(".requestacces").each(function (i) {
+$('.requestacces').each(function (i) {
   var animateIn = new TimelineMax();
-  var imgOvarly = $(this).find("p span, a");
+  var imgOvarly = $(this).find('p span, a');
   console.log(imgOvarly);
   animateIn.staggerFromTo(
     imgOvarly,
@@ -186,13 +194,13 @@ $(".requestacces").each(function (i) {
     {
       opacity: 0,
       y: 30,
-      transformOrigin: "bottom",
+      transformOrigin: 'bottom',
       ease: Power4.easeOut,
     },
     {
       opacity: 1,
       y: 0,
-      transformOrigin: "bottom",
+      transformOrigin: 'bottom',
       ease: Power4.easeOut,
     },
     0.5
@@ -201,7 +209,7 @@ $(".requestacces").each(function (i) {
   var scene = new ScrollMagic.Scene({
     triggerElement: this,
     triggerHook: 0.5,
-    offset: "0",
+    offset: '0',
   })
     .setTween(animateIn)
     .addIndicators()
@@ -210,175 +218,175 @@ $(".requestacces").each(function (i) {
   var reverse = scene.reverse();
   scene.reverse(true);
 });
-
-
 
 /* slider */
 // setup variable
-const slider_one = $(".testimonial-one .slide"),
-  slider1 = $(".testimonial-one .slider1"),
-  slider2 = $(".testimonial-one .slider2"),
-  slider3 = $(".testimonial-one .slider3"),
-  actionLeft = $(".testimonial-action .action-left"),
-  actionRight = $(".testimonial-action .action-right"),
-  active = $(".testimonial-one .active");
 
-function init() {
-  TweenLite.set(slider_one.not(active), {
-    autoAlpha: 0,
-  });
+if ($('body').hasClass('.testimonial-one')) {
+  const slider_one = $('.testimonial-one .slide'),
+    slider1 = $('.testimonial-one .slider1'),
+    slider2 = $('.testimonial-one .slider2'),
+    slider3 = $('.testimonial-one .slider3'),
+    actionLeft = $('.testimonial-action .action-left'),
+    actionRight = $('.testimonial-action .action-right'),
+    active = $('.testimonial-one .active');
 
-  TweenLite.set(actionLeft, {
-    autoAlpha: 0.3,
-    color: "#444",
-  });
-
-  TweenLite.set(actionRight, {
-    autoAlpha: 1,
-    color: "#fff",
-  });
-}
-init();
-
-function gotoNextSlider(slideOut, slideIn) {
-  const tl = gsap.timeline(),
-    content = slideIn.find("blockquote"),
-    author = slideIn.find("footer"),
-    index = slideIn.index(),
-    size = $(".testimonial-one .slide").length;
-
-  if (slideIn !== 0) {
-    // go to next slider
-    tl
-      // init out
-      .set(slideOut, {
-        autoAlpha: 0,
-        css: {
-          className: "-=slide testimonial-content",
-        },
-      })
-      // init in
-      .set(slideIn, {
-        y: "100%",
-        autoAlpha: 0,
-        css: {
-          className: "+=slide active testimonial-content",
-        },
-      })
-      // move out
-      .to(
-        slideOut,
-        {
-          autoAlpha: 0,
-          y: "-100%",
-          ease: Power3.easeInOut,
-        },
-        0
-      )
-      //move in
-      .to(
-        slideIn,
-        {
-          y: "0",
-          autoAlpha: 1,
-          ease: Power3.easeInOut,
-        },
-        0
-      );
-  }
-
-  TweenLite.to(actionLeft, 0.3, {
-    autoAlpha: 1,
-    color: "#fff",
-  });
-
-  console.log(index + "," + size);
-
-  if (index === 1) {
-    TweenLite.to(actionRight, 0.3, {
+  function init() {
+    TweenLite.set(slider_one.not(active), {
       autoAlpha: 0,
-      color: "#444",
+    });
+
+    TweenLite.set(actionLeft, {
+      autoAlpha: 0.3,
+      color: '#444',
+    });
+
+    TweenLite.set(actionRight, {
+      autoAlpha: 1,
+      color: '#fff',
     });
   }
-}
+  init();
 
-actionRight.click(function (e) {
-  e.preventDefault();
-  const slideOut = $(".testimonial-one .slide"),
-    slideIn = $(".testimonial-one .slide.active").next();
-  gotoNextSlider(slideOut, slideIn);
-});
+  function gotoNextSlider(slideOut, slideIn) {
+    const tl = gsap.timeline(),
+      content = slideIn.find('blockquote'),
+      author = slideIn.find('footer'),
+      index = slideIn.index(),
+      size = $('.testimonial-one .slide').length;
 
-function gotoPrevSlider(slideOut, slideIn) {
-  console.log("left");
-
-  const tl = gsap.timeline(),
-    content = slideIn.find("blockquote"),
-    author = slideIn.find("footer"),
-    index = slideIn.index(),
-    size = $(".testimonial-one .slide").length;
-
-  console.log(index + "," + size);
-
-  if (slideIn !== 0) {
-    // go to prev slider
-    tl
-      // init out
-      .set(slideOut, {
-        autoAlpha: 0,
-        css: {
-          className: "-=slide testimonial-content",
-        },
-      })
-      // init in
-      .set(slideIn, {
-        y: "-100%",
-        autoAlpha: 0,
-        css: {
-          className: "+=slide active testimonial-content",
-        },
-      })
-      // move out
-      .to(
-        slideOut,
-        {
+    if (slideIn !== 0) {
+      // go to next slider
+      tl
+        // init out
+        .set(slideOut, {
           autoAlpha: 0,
-          y: "100%",
-          ease: Power3.easeInOut,
-        },
-        0
-      )
-      //move in
-      .to(
-        slideIn,
-        {
-          y: "0",
-          autoAlpha: 1,
-          ease: Power3.easeInOut,
-        },
-        0
-      );
-  }
+          css: {
+            className: '-=slide testimonial-content',
+          },
+        })
+        // init in
+        .set(slideIn, {
+          y: '100%',
+          autoAlpha: 0,
+          css: {
+            className: '+=slide active testimonial-content',
+          },
+        })
+        // move out
+        .to(
+          slideOut,
+          {
+            autoAlpha: 0,
+            y: '-100%',
+            ease: Power3.easeInOut,
+          },
+          0
+        )
+        //move in
+        .to(
+          slideIn,
+          {
+            y: '0',
+            autoAlpha: 1,
+            ease: Power3.easeInOut,
+          },
+          0
+        );
+    }
 
-  TweenLite.to(actionRight, 0.3, {
-    autoAlpha: 1,
-    color: "white",
-  });
-
-  if (index === 0) {
     TweenLite.to(actionLeft, 0.3, {
-      autoAlpha: 0,
+      autoAlpha: 1,
+      color: '#fff',
     });
+
+    console.log(index + ',' + size);
+
+    if (index === 1) {
+      TweenLite.to(actionRight, 0.3, {
+        autoAlpha: 0,
+        color: '#444',
+      });
+    }
   }
+
+  actionRight.click(function (e) {
+    e.preventDefault();
+    const slideOut = $('.testimonial-one .slide'),
+      slideIn = $('.testimonial-one .slide.active').next();
+    gotoNextSlider(slideOut, slideIn);
+  });
+
+  function gotoPrevSlider(slideOut, slideIn) {
+    console.log('left');
+
+    const tl = gsap.timeline(),
+      content = slideIn.find('blockquote'),
+      author = slideIn.find('footer'),
+      index = slideIn.index(),
+      size = $('.testimonial-one .slide').length;
+
+    console.log(index + ',' + size);
+
+    if (slideIn !== 0) {
+      // go to prev slider
+      tl
+        // init out
+        .set(slideOut, {
+          autoAlpha: 0,
+          css: {
+            className: '-=slide testimonial-content',
+          },
+        })
+        // init in
+        .set(slideIn, {
+          y: '-100%',
+          autoAlpha: 0,
+          css: {
+            className: '+=slide active testimonial-content',
+          },
+        })
+        // move out
+        .to(
+          slideOut,
+          {
+            autoAlpha: 0,
+            y: '100%',
+            ease: Power3.easeInOut,
+          },
+          0
+        )
+        //move in
+        .to(
+          slideIn,
+          {
+            y: '0',
+            autoAlpha: 1,
+            ease: Power3.easeInOut,
+          },
+          0
+        );
+    }
+
+    TweenLite.to(actionRight, 0.3, {
+      autoAlpha: 1,
+      color: 'white',
+    });
+
+    if (index === 0) {
+      TweenLite.to(actionLeft, 0.3, {
+        autoAlpha: 0,
+      });
+    }
+  }
+
+  actionLeft.click(function (e) {
+    e.preventDefault();
+    const slideOut = $('.testimonial-one .slide'),
+      slideIn = $('.testimonial-one .slide.active').prev();
+    gotoPrevSlider(slideOut, slideIn);
+  });
 }
-
-actionLeft.click(function (e) {
-  e.preventDefault();
-  const slideOut = $(".testimonial-one .slide"),
-    slideIn = $(".testimonial-one .slide.active").prev();
-  gotoPrevSlider(slideOut, slideIn);
-});
-
 
 // /* wave pattern */
 // /*
