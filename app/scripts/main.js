4fe5a1ffdb5fb9f96b29a092c2ce5bc5a023225d
kavinrajsi <we@sikavinraj.me>
2020-04-29 10:42:24 +0530

    updated footer button, works list animation & spacing, alignment on header and hero text

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -178,7 +178,7 @@ $(".project-list").each(function (i) {
 /* last section */
 $(".requestacces").each(function (i) {
   var animateIn = new TimelineMax();
-  var imgOvarly = $(this).find("p span, a");
+  var imgOvarly = $(this).find("p span");
 
   animateIn.staggerFromTo(
     imgOvarly,
@@ -215,170 +215,327 @@ $(".requestacces").each(function (i) {
 
 /* slider */
 // setup variable
-// if ($(window).width() < 768) {
-  // alert('Less than 768');
-const slider_one = $(" .testimonial-one .slide"),
-  actionLeft = $(" .testimonial-action .action-left"),
-  actionRight = $(" .testimonial-action .action-right"),
-  active = $(" .testimonial-one .active");
+// if ($(".hear-them-say-it").hasClass("view-mobile")){
+if (($(window).width() < 768 )){
+  const slider_one = $(".view-mobile .testimonial-one .slide"),
+    actionLeft = $(".view-mobile .testimonial-action .action-left"),
+    actionRight = $(".view-mobile .testimonial-action .action-right"),
+    active = $(".view-mobile .testimonial-one .active");
 
-function init() {
-  TweenLite.set(slider_one.not(active), {
-    autoAlpha: 0,
-  });
+  function init() {
+    TweenLite.set(slider_one.not(active), {
+      autoAlpha: 0,
+    });
 
-  TweenLite.set(actionLeft, {
-    autoAlpha: 0.3,
-    color: "#444",
-  });
+    TweenLite.set(actionLeft, {
+      autoAlpha: 0.3,
+      color: "#444",
+    });
 
-  TweenLite.set(actionRight, {
-    autoAlpha: 1,
-    color: "#fff",
-  });
-}
-init();
-
-function gotoNextSlider(slideOut, slideIn) {
-  const tl = gsap.timeline(),
-    content = slideIn.find("blockquote"),
-    author = slideIn.find("footer"),
-    index = slideIn.index(),
-    size = $(" .testimonial-one .slide").length;
-
-  if (slideIn !== 0) {
-    // go to next slider
-    tl
-      // init out
-      .set(slideOut, {
-        autoAlpha: 0,
-        css: {
-          className: "-=slide testimonial-content",
-        },
-      })
-      // init in
-      .set(slideIn, {
-        y: "100%",
-        autoAlpha: 0,
-        css: {
-          className: "+=slide active testimonial-content",
-        },
-      })
-      // move out
-      .to(
-        slideOut,
-        {
-          autoAlpha: 0,
-          y: "-100%",
-          ease: Power3.easeInOut,
-        },
-        0
-      )
-      //move in
-      .to(
-        slideIn,
-        {
-          y: "0",
-          autoAlpha: 1,
-          ease: Power3.easeInOut,
-        },
-        0
-      );
+    TweenLite.set(actionRight, {
+      autoAlpha: 1,
+      color: "#fff",
+    });
   }
+  init();
 
-  // TweenLite.to(actionLeft, 0.3, {
-  //   autoAlpha: 1,
-  //   color: "#fff",
-  // });
+  function gotoNextSlider(slideOut, slideIn) {
+    const tl = gsap.timeline(),
+      content = slideIn.find("blockquote"),
+      author = slideIn.find("footer"),
+      index = slideIn.index(),
+      size = $(".view-mobile .testimonial-one .slide").length;
 
-  console.log(index + "," + size);
+    if (slideIn !== 0) {
+      // go to next slider
+      tl
+        // init out
+        .set(slideOut, {
+          autoAlpha: 0,
+          css: {
+            className: "-=slide testimonial-content",
+          },
+        })
+        // init in
+        .set(slideIn, {
+          y: "100%",
+          autoAlpha: 0,
+          css: {
+            className: "+=slide active testimonial-content",
+          },
+        })
+        // move out
+        .to(
+          slideOut,
+          {
+            autoAlpha: 0,
+            y: "-100%",
+            ease: Power3.easeInOut,
+          },
+          0
+        )
+        //move in
+        .to(
+          slideIn,
+          {
+            y: "0",
+            autoAlpha: 1,
+            ease: Power3.easeInOut,
+          },
+          0
+        );
+    }
 
-  // if (index === 1) {
+    // TweenLite.to(actionLeft, 0.3, {
+    //   autoAlpha: 1,
+    //   color: "#fff",
+    // });
+
+    console.log(index + "," + size);
+
+    // if (index === 1) {
     // TweenLite.to(actionRight, 0.3, {
     //   autoAlpha: 0,
     //   color: "#444",
     // });
-  // }
-}
+    // }
+  }
 
   actionRight.click(function (e) {
     console.log('right');
-  e.preventDefault();
-  const slideOut = $(" .testimonial-one .slide"),
-    slideIn = $(" .testimonial-one .slide.active").next();
-  gotoNextSlider(slideOut, slideIn);
-});
+    e.preventDefault();
+    const slideOut = $(".view-mobile .testimonial-one .slide"),
+      slideIn = $(".view-mobile .testimonial-one .slide.active").next();
+    gotoNextSlider(slideOut, slideIn);
+  });
 
-function gotoPrevSlider(slideOut, slideIn) {
-  console.log("left");
+  function gotoPrevSlider(slideOut, slideIn) {
+    console.log("left");
 
-  const tl = gsap.timeline(),
-    content = slideIn.find("blockquote"),
-    author = slideIn.find("footer"),
-    index = slideIn.index(),
-    size = $(" .testimonial-one .slide").length;
+    const tl = gsap.timeline(),
+      content = slideIn.find("blockquote"),
+      author = slideIn.find("footer"),
+      index = slideIn.index(),
+      size = $(".view-mobile .testimonial-one .slide").length;
 
-  console.log(index + "," + size);
+    console.log(index + "," + size);
 
-  if (slideIn !== 0) {
-    // go to prev slider
-    tl
-      // init out
-      .set(slideOut, {
-        autoAlpha: 0,
-        css: {
-          className: "-=slide testimonial-content",
-        },
-      })
-      // init in
-      .set(slideIn, {
-        y: "-100%",
-        autoAlpha: 0,
-        css: {
-          className: "+=slide active testimonial-content",
-        },
-      })
-      // move out
-      .to(
-        slideOut,
-        {
+    if (slideIn !== 0) {
+      // go to prev slider
+      tl
+        // init out
+        .set(slideOut, {
           autoAlpha: 0,
-          y: "100%",
-          ease: Power3.easeInOut,
-        },
-        0
-      )
-      //move in
-      .to(
-        slideIn,
-        {
-          y: "0",
-          autoAlpha: 1,
-          ease: Power3.easeInOut,
-        },
-        0
-      );
-  }
+          css: {
+            className: "-=slide testimonial-content",
+          },
+        })
+        // init in
+        .set(slideIn, {
+          y: "-100%",
+          autoAlpha: 0,
+          css: {
+            className: "+=slide active testimonial-content",
+          },
+        })
+        // move out
+        .to(
+          slideOut,
+          {
+            autoAlpha: 0,
+            y: "100%",
+            ease: Power3.easeInOut,
+          },
+          0
+        )
+        //move in
+        .to(
+          slideIn,
+          {
+            y: "0",
+            autoAlpha: 1,
+            ease: Power3.easeInOut,
+          },
+          0
+        );
+    }
 
-  // TweenLite.to(actionRight, 0.3, {
-  //   autoAlpha: 1,
-  //   color: "white",
-  // });
+    // TweenLite.to(actionRight, 0.3, {
+    //   autoAlpha: 1,
+    //   color: "white",
+    // });
 
-  // if (index === 0) {
+    // if (index === 0) {
     // TweenLite.to(actionLeft, 0.3, {
     //   autoAlpha: 0,
     // });
-  // }
+    // }
+  }
+
+  actionLeft.click(function (e) {
+    e.preventDefault();
+    const slideOut = $(".view-mobile .testimonial-one .slide"),
+      slideIn = $(".view-mobile .testimonial-one .slide.active").prev();
+    gotoPrevSlider(slideOut, slideIn);
+  });
+
+  console.log('mobile');
+} else {
+  console.log('den');
+  const slider_one = $(".view-desktop .testimonial-one .slide"),
+    actionLeft = $(".view-desktop .testimonial-action .action-left"),
+    actionRight = $(".view-desktop .testimonial-action .action-right"),
+    active = $(".view-desktop .testimonial-one .active");
+
+  function init() {
+    TweenLite.set(slider_one.not(active), {
+      autoAlpha: 0,
+    });
+
+    TweenLite.set(actionLeft, {
+      css:{'pointer-events':'none', color: '#fff', opacity: 0.4 }
+    });
+
+    TweenLite.set(actionRight, {
+      css:{color: '#fff', opacity: 1.0 ,'pointer-events':'initial' }
+    });
+  }
+  init();
+
+  function gotoNextSlider(slideOut, slideIn) {
+    const tl = gsap.timeline(),
+      content = slideIn.find("blockquote"),
+      author = slideIn.find("footer"),
+      index = slideIn.index(),
+      size = $(".view-desktop .testimonial-one .slide").length;
+
+    if (slideIn !== 0) {
+      // go to next slider
+      tl
+        // init out
+        .set(slideOut, {
+          autoAlpha: 0,
+          css: {
+            className: "-=slide testimonial-content",
+          },
+        })
+        // init in
+        .set(slideIn, {
+          y: "100%",
+          autoAlpha: 0,
+          css: {
+            className: "+=slide active testimonial-content",
+          },
+        })
+        // move out
+        .to(
+          slideOut,
+          {
+            autoAlpha: 0,
+            y: "-100%",
+            ease: Power3.easeInOut,
+          },
+          0
+        )
+        //move in
+        .to(
+          slideIn,
+          {
+            y: "0",
+            autoAlpha: 1,
+            ease: Power3.easeInOut,
+          },
+          0
+        );
+    }
+
+    TweenLite.to(actionLeft, 0.3, {
+      css:{color: '#fff', opacity: 1.0 ,'pointer-events':'initial' }
+    });
+
+    console.log(index);
+
+    if (index == 1) {
+    TweenLite.to(actionRight, 0.3, {
+      css:{'pointer-events':'none', color: '#fff', opacity: 0.4 }
+    });
+    }
+  }
+
+  actionRight.click(function (e) {
+    console.log('right');
+    e.preventDefault();
+    const slideOut = $(".view-desktop .testimonial-one .slide"),
+      slideIn = $(".view-desktop .testimonial-one .slide.active").next();
+    gotoNextSlider(slideOut, slideIn);
+  });
+
+  function gotoPrevSlider(slideOut, slideIn) {
+    console.log("left");
+
+    const tl = gsap.timeline(),
+      content = slideIn.find("blockquote"),
+      author = slideIn.find("footer"),
+      index = slideIn.index(),
+      size = $(".view-desktop .testimonial-one .slide").length;
+
+    console.log(index + "," + size);
+
+    if (slideIn !== 0) {
+      // go to prev slider
+      tl
+        // init out
+        .set(slideOut, {
+          autoAlpha: 0,
+          css: {
+            className: "-=slide testimonial-content",
+          },
+        })
+        // init in
+        .set(slideIn, {
+          y: "-100%",
+          autoAlpha: 0,
+          css: {
+            className: "+=slide active testimonial-content",
+          },
+        })
+        // move out
+        .to(
+          slideOut,
+          {
+            autoAlpha: 0,
+            y: "100%",
+            ease: Power3.easeInOut,
+          },
+          0
+        )
+        //move in
+        .to(
+          slideIn,
+          {
+            y: "0",
+            autoAlpha: 1,
+            ease: Power3.easeInOut,
+          },
+          0
+        );
+    }
+
+    TweenLite.to(actionRight, 0.3, {
+      css:{color: '#fff', opacity: 1.0 ,'pointer-events':'initial' }
+    });
+
+    if (index === 0) {
+    TweenLite.to(actionLeft, 0.3, {
+      css:{'pointer-events':'none', color: '#fff', opacity: 0.4 }
+    });
+    }
+  }
+
+  actionLeft.click(function (e) {
+    e.preventDefault();
+    const slideOut = $(".view-desktop .testimonial-one .slide"),
+      slideIn = $(".view-desktop .testimonial-one .slide.active").prev();
+    gotoPrevSlider(slideOut, slideIn);
+  });
 }
-
-actionLeft.click(function (e) {
-  e.preventDefault();
-  const slideOut = $(" .testimonial-one .slide"),
-    slideIn = $(" .testimonial-one .slide.active").prev();
-  gotoPrevSlider(slideOut, slideIn);
-});
-
-// }else {
-//   // alert('More than 960');
-// }
