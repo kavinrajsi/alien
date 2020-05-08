4378f3a7b6a41a277c70496a60092b820c41452e
kavinrajsi <we@sikavinraj.me>
2020-05-08 13:51:02 +0530

    we let our work bg color:

--- app/tryme.php
+++ app/tryme.php
@@ -1,5 +1,6 @@
 <?php
 // the message
+$service = ($_POST['service[]']);
 $name = ($_POST['name']);
 $email = ($_POST['email']);
 $mobile = ($_POST['phoneNumber']);
@@ -8,14 +9,20 @@ $hearAboutus = ($_POST['hearAboutus']);
 
 $to = "sikavinraj@gmail.com";
 
-$subject = "[Arise Academy] Enquire by ". $name;
+$subject = "[Alien Design Contact form] Enquire by ". $name." for $service;
 
 $msg = "<html><body style='font-family:Lato,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Enquire for Arise Academy</h2>\r\n";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Enquire for theAlien.design</h2>\r\n";
 $msg .= "<table width='600' border='0' align='left' cellpadding='0' cellspacing='0' bgcolor='#FFF' style='font-weight: 700;font-size: 12px;color: #666;font-family:Arial, Helvetica, sans-serif; background-color:#FFFFFF;'>";
 $msg .= " <tr><td width='250' style='border: 1px solid #CCC; border-bottom:none; border-right:none; padding:10px;'>Name</td><td width='250' style='border: 1px solid #CCC; border-bottom:none; padding:10px;'> " . $name . "</td></tr>";
 $msg .= " <tr><td width='250' style='border: 1px solid #CCC; border-bottom:none; border-right:none; padding:10px;'>Email</td><td width='250' style='border: 1px solid #CCC; border-bottom:none; padding:10px;'> " . $email . "</td></tr>";
 $msg .= " <tr><td width='250' style='border: 1px solid #CCC; border-right:none; padding:10px;'>Phone Number</td><td width='250' style='border: 1px solid #CCC; padding:10px;'>" . $mobile . "</td></tr>";
+$msg .= "</table>";
+$msg .= " <tr><td width='250' style='border: 1px solid #CCC; border-right:none; padding:10px;'>Service</td><td width='250' style='border: 1px solid #CCC; padding:10px;'>" . $service . "</td></tr>";
+$msg .= "</table>";
+$msg .= " <tr><td width='250' style='border: 1px solid #CCC; border-right:none; padding:10px;'>Project Detail</td><td width='250' style='border: 1px solid #CCC; padding:10px;'>" . $projectDetails . "</td></tr>";
+$msg .= "</table>";
+$msg .= " <tr><td width='250' style='border: 1px solid #CCC; border-right:none; padding:10px;'>How did you hear about us</td><td width='250' style='border: 1px solid #CCC; padding:10px;'>" . $hearAboutus . "</td></tr>";
 $msg .= "</table>";
 $msg .= "</body></html>";
 
